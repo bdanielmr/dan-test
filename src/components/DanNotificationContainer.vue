e54db419eaa73a2fdd11e3877e0eaaf51fbad782
danielmr <50649935+bdanielmr@users.noreply.github.com>
2020-11-20 08:20:48 -0500

    add notification module when success and fetch

---
+++ src/components/DanNotificationContainer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="notification-container">
+    <dan-notificationBar
+      v-for="notification in this.$store.state.notification.notifications"
+      :key="notification.id"
+      :notification="notification"
+    />
+
+  </div>
+</template>
+
+<script>
+import DanNotificationBar from '@/components/DanNotificationBar.vue'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    DanNotificationBar
+  },
+  computed: {
+    ...mapState(['notification/notifications'])
+    }
+}
+</script>
+
+<style>
+.notification-container {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  padding-right: 40px;
+  padding-bottom: 50px;
+}
+</style>
